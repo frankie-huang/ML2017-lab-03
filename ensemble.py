76fb888dfe3b2ff0cd586b23c7a1aa587a8a8c85
frankie <1298715080@qq.com>
2017-12-22 11:36:31 +0800

    updat

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,7 @@
+#encoding=utf-8
 import pickle
+import numpy as np
+from sklearn.metrics import classification_report 
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
@@ -10,7 +13,8 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
-        pass
+        self.weak_classifier=weak_classifier
+        self.n_weakers_limit=n_weakers_limit
 
     def is_good_enough(self):
         '''Optional'''
@@ -23,8 +27,36 @@ class AdaBoostClassifier:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
+        self.alpha=[]
+        self.classifier=[]
+        w=np.ones(len(X))/len(X)#样本权重
+        for i in range(self.n_weakers_limit):
+            print('正在训练第',i,'个模型')
+            if i==0:
+                res=np.zeros(len(X))-1
+            else:
+                res=self.classifier[-1].predict(X)
+            
+            self.classifier.append(self.weak_classifier(max_depth=1).fit(X,y,w))
+            
+            #计算错误率
+            err=0.0
+            for j in range(len(X)):
+                if self.classifier[i].predict(X[j].reshape(1,-1))!=y[j]:
+                    err+=w[j]#在weight分布下的错误率
+            if err>0.5:
+                break
+            #计算alpha
+            self.alpha.append(1.0/2*np.log(1/err-1))
 
+            #更新w
+            for k in range(len(X)):
+                w[k]=w[k]*np.exp(-y[k]*self.alpha[i]*self.classifier[i].predict(X[k].reshape(1,-1)))
+            
+            #归一化
+            s=sum(w)#规范化因子
+            w/=s
+        
 
     def predict_scores(self, X):
         '''Calculate the weighted sum score of the whole base classifiers for given samples.
@@ -47,7 +79,17 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        labels=[]
+        for j in range(len(X)):
+            res=0.0
+            for i in range(len(self.alpha)):
+                res+=self.alpha[i]*(self.classifier[i].predict(X[j].reshape(1,-1)))
+            if res >= threshold :
+                labels.append(1)
+            else:
+                labels.append(-1)
+        return labels
+        
 
     @staticmethod
     def save(model, filename):
--- train.py
+++ train.py
@@ -1,6 +1,38 @@
-
+#encoding=utf-8
+from feature import *
+from ensemble import *
+from PIL import Image
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report 
+from sklearn.tree import DecisionTreeClassifier
 
 if __name__ == "__main__":
-    # write your code here
-    pass
-
+    data=[]
+    for i in range(500):
+        x=str(i)
+        if i<10:
+            x='00'+x
+        elif i<100:
+            x='0'+x
+        im1 = Image.open('datasets/original/face/face_'+x+'.jpg').resize((24,24)).convert('L')
+        im2 = Image.open('datasets/original/nonface/nonface_'+x+'.jpg').resize((24,24)).convert('L')        
+        data.append(NPDFeature(np.array(im1)).extract())
+        data.append(NPDFeature(np.array(im2)).extract())        
+    label=[]
+    # #缓存特征
+    # AdaBoostClassifier.save(data,'data')
+    # #读取缓存的特征
+    # data=np.array(AdaBoostClassifier.load('data'))
+    for i in range(1000):
+        if i%2==0:
+            label.append(1)
+        else:
+            label.append(-1)
+    X_train,X_test,y_train,y_test = train_test_split(data,label,test_size=0.2,random_state=100)
+    classifier=AdaBoostClassifier(DecisionTreeClassifier,20)
+    classifier.fit(X_train,y_train)
+    res_train=classifier.predict(X_train)
+    res_test=classifier.predict(X_test)
+    print(classification_report(y_train,res_train))
+    print(classification_report(y_test,res_test))
